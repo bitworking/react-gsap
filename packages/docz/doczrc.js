e941b2ebcd1970d1827454dfb20a4b6ab50d2eb2
Jan Fischer <jan.fischer@nexum.de>
2020-05-05 16:27:11 +0200

    add base path to docz config

--- packages/docz/doczrc.js
+++ packages/docz/doczrc.js
@@ -1,5 +1,6 @@
 export default {
   typescript: true,
+  base: '/react-gsap/',
   title: 'react-gsap Docs',
   description:
     'react-gsap lets you use the GreenSock Animation Platform (GSAP) in React in a fully declarative way. It abstracts away the direct use of the GSAP Tween and Timeline functions.',
