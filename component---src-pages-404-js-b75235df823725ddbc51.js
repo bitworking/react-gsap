6e3fc1841218d9afcbe0a379e563e0b6e63470c6
Jan Fischer <jan.fischer@nexum.de>
2020-05-05 16:18:54 +0200

    Updates

---
+++ component---src-pages-404-js-b75235df823725ddbc51.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{w2l6:function(t,e,n){"use strict";n.r(e);var i=n("xH0s");e.default=function(){return Object(i.c)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-b75235df823725ddbc51.js.map
---
+++ styles-e49123c686ea18d13d39.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[17],[]]);
+//# sourceMappingURL=styles-e49123c686ea18d13d39.js.map
